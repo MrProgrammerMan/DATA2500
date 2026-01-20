17dea2ce7ae6e268f5c2d58d4734161d40b4728c
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-01-20 09:36:25 +0100

    feat(tex): set up texlive for pdf generation

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs @ { flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" ];
+    perSystem = { config, pkgs, ... }: {
+      packages = 
+        let 
+          tex = (pkgs.texlive.withPackages (ps: with ps;[ latex-bin latexmk ]));
+        in {
+          oblig1 = pkgs.stdenv.mkDerivation {
+            name = "Obligatory assigment 1 - DATA2500";
+            src = ./04-excercises/oblig1;
+            buildInputs = [ tex ];
+            phases = [ "buildPhase" "installPhase"];
+            buildPhase = ''
+              latexmk -pdf -interaction=nonstopmode $src/document.tex
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp document.pdf $out/
+            '';
+          };
+      };
+    };
+  };
+}
