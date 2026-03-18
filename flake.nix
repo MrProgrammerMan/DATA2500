f10b78a4f530deae56136aa920df1fc61013806e
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-03-18 20:00:29 +0100

    feat(flake): direnv

--- flake.nix
+++ flake.nix
@@ -5,30 +5,34 @@
   };
   outputs = inputs @ { flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" ];
-    perSystem = { config, pkgs, ... }: {
-      packages = 
-        let 
-          tex = (pkgs.texlive.withPackages (ps: with ps;[ 
-            latexmk
-            scheme-small
-            ps.import
-          ]));
-        in {
-          oblig1 = pkgs.stdenv.mkDerivation {
-            name = "Obligatory assigment 1 - DATA2500";
-            src = ./04-excercises/oblig1;
-            buildInputs = [ tex ];
-            phases = [ "buildPhase" "installPhase"];
-            buildPhase = ''
-              cp -r $src/* . #This is hella dumb, but include doesn't work otherwise
-              latexmk -pdf -interaction=nonstopmode main.tex
-            '';
-            installPhase = ''
-              mkdir -p $out
-              cp main.pdf $out/
-            '';
-          };
-      };
+    perSystem = { config, pkgs, ... }:
+      let 
+        tex = (pkgs.texlive.withPackages (ps: with ps;[ 
+          latexmk
+          scheme-small
+          ps.import
+        ]));
+      in {
+        packages.oblig1 = pkgs.stdenv.mkDerivation {
+          name = "Obligatory assigment 1 - DATA2500";
+          src = ./04-excercises/oblig1;
+          buildInputs = [ tex ];
+          phases = [ "buildPhase" "installPhase"];
+          buildPhase = ''
+            cp -r $src/* . #This is hella dumb, but include doesn't work otherwise
+            latexmk -pdf -interaction=nonstopmode main.tex
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp main.pdf $out/
+          '';
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ tex ];
+          shellHook = ''
+            latexmk -pdf -pvc $PWD/04-excercises/oblig1/main.tex
+          '';
+        };
     };
   };
 }
